d0655772f18d92df3e08c9ebad5a2b6e2f6de9f0
LaSe28 <mattia.las28@gmail.com>
2022-03-02 19:57:38 +0100

    end

--- js/script.js
+++ js/script.js
@@ -2,55 +2,69 @@ let easyGrill = document.querySelector('.griglia-easy')
 let mediumGrill = document.querySelector('.griglia-medium')
 let hardGrill = document.querySelector('.griglia-hard')
 let btnPlay = document.querySelector('#play')
+let text = document.querySelector('.text')
 let sum = 0;
-let square;
 
 let difficulty = document.querySelector('#difficulty');
 
 
-difficulty.addEventListener('change' , function(){
 
-    btnPlay.addEventListener('click', function(){
-        if (difficulty.value == 'easy'){
-            easyGrill.classList.add('show')
-            mediumGrill.classList.remove('show')
-            hardGrill.classList.remove('show')
-            for ( e = 1; e <= 49; e++){
-                sum += e
-                square = document.createElement(`div`);
-                square.classList.add('box')
-                square.innerHTML= e
-                easyGrill.append(square)
-            }
+btnPlay.addEventListener('click', function(){
+    if (difficulty.value == 'easy'){
+        text.classList.add('hidden')
+        easyGrill.innerHTML=''
+        easyGrill.classList.add('show')
+        mediumGrill.classList.remove('show')
+        hardGrill.classList.remove('show')
+        
+        for ( e = 1; e <= 49; e++){
+            sum += e
+            let square = document.createElement(`div`);
+            square.classList.add('box')
+            square.innerHTML= e
+            square.addEventListener('click', function(){
+                square.classList.add('active')
+            })
+            easyGrill.append(square)
         }
-        if (difficulty.value == 'medium'){
-            easyGrill.classList.remove('show')
-            mediumGrill.classList.add('show')
-            hardGrill.classList.remove('show')
+    }
+    if (difficulty.value == 'medium'){
+        text.classList.add('hidden')
+        mediumGrill.innerHTML=''
+        easyGrill.classList.remove('show')
+        mediumGrill.classList.add('show')
+        hardGrill.classList.remove('show')
 
-            for ( m = 1; m <= 81; m++){
-                sum += m
-                square = document.createElement(`div`);
-                square.classList.add('box')
-                square.innerHTML= m
-                mediumGrill.append(square)
-            }
+        for ( m = 1; m <= 81; m++){
+            sum += m
+            let square = document.createElement(`div`);
+            square.classList.add('box')
+            square.innerHTML= m
+            square.addEventListener('click', function(){
+                square.classList.add('active')
+            })
+            mediumGrill.append(square)
         }
-        if (difficulty.value == 'hard'){
-            easyGrill.classList.remove('show')
-            mediumGrill.classList.remove('show')
-            hardGrill.classList.add('show')
-    
-            for ( h = 1; h <= 100; h++){
-                sum += h
-                square = document.createElement(`div`);
-                square.classList.add('box')
-                square.innerHTML= h
-                hardGrill.append(square)
-            }
+    }
+    if (difficulty.value == 'hard'){
+        text.classList.add('hidden')
+        hardGrill.innerHTML=''
+        easyGrill.classList.remove('show')
+        mediumGrill.classList.remove('show')
+        hardGrill.classList.add('show')
+
+        for ( h = 1; h <= 100; h++){
+            sum += h
+            let square = document.createElement(`div`);
+            square.classList.add('box')
+            square.innerHTML= h
+            square.addEventListener('click', function(){
+                square.classList.add('active')
+            })
+            hardGrill.append(square)
         }
-    })
-})
+    }
+});
 
 
 
